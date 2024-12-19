56a4560ace08e6709293943bd1f41170d4890e02
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 10:28:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://mern-ecommerce-store-1-ycta.onrender.com",
+      "/uploads/": "https://mern-ecommerce-store-1-ycta.onrender.com",
     },
   },
 });
