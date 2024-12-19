372d721f3bf84f914add74e9abf6f043e676d6fc
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 11:31:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,26 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": process.env.VITE_BACKEND_URL,
-      "/uploads/": process.env.VITE_BACKEND_URL,
+      "/api/": "https://mern-ecommerce-store-1-ycta.onrender.com",
+      "/uploads/": "https://mern-ecommerce-store-1-ycta.onrender.com",
     },
   },
-  build: {
-    outDir: "dist", 
-    minify: "terser", 
-    target: "esnext", 
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"], 
-        },
-      },
-    },
-    sourcemap: false, 
-  },
-
-  define: {
-    "process.env.NODE_ENV": '"production"',
-  },
-});
-});
+}); 
