c4fdf4f345c51416798c45846e4377f9ac324c4a
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 11:14:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,4 +12,22 @@ export default defineConfig({
       "/uploads/": "https://mern-ecommerce-store-1-ycta.onrender.com",
     },
   },
+  build: {
+    outDir: "dist", 
+    minify: "terser", 
+    target: "esnext", 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], 
+        },
+      },
+    },
+    sourcemap: false, 
+  },
+
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+});
 });
